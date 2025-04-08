ce1ea2ba113935065a04e4b10bdac49e516bbf86
Ashish Bhusal <ashish.bhusal.16999@gmail.com>
2025-04-07 17:02:45 -0700

    Initialise the Spring Boot Backend Application

---
+++ booktable-backend/src/main/java/com/booktable/BooktableBackendApplication.java
@@ -0,0 +1,13 @@
+package com.booktable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktableBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktableBackendApplication.class, args);
+	}
+
+}
