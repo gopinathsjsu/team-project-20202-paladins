d18ba5bac3ec4d50870a03afc1e6c981e00201cf
nishan <nishan.paudel1914@gmail.com>
2025-04-11 14:44:26 -0700

    User Repo change

--- booktable-backend/src/main/java/com/booktable/controller/AuthController.java
+++ booktable-backend/src/main/java/com/booktable/controller/AuthController.java
@@ -1,4 +1,14 @@
 package com.booktable.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class AuthController {
+
+    @GetMapping("/api/hello2")
+    public String sayHello() {
+        return "Hello from BookTable!";
+    }
 }
--- booktable-backend/src/main/java/com/booktable/model/User.java
+++ booktable-backend/src/main/java/com/booktable/model/User.java
@@ -1,4 +1,49 @@
 package com.booktable.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "users") // For MongoDB
 public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setId("1");
+        user.setUsername("testuser");
+        user.setPassword("password123");
+
+        System.out.println("User ID: " + user.getId());
+        System.out.println("Username: " + user.getUsername());
+        System.out.println("Password: " + user.getPassword());
+    }
 }
