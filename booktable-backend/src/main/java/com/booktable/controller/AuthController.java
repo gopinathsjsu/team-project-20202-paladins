b544bb1b6a0f5d9ead6aedff2ffd2c489284a883
Ashish Bhusal <ashish.bhusal.16999@gmail.com>
2025-04-11 15:46:03 -0700

    Backend: Add user registration controller and AuthService
    Assign CUSTOMER role by default, add proper status codes if user is
    already present.

--- booktable-backend/src/main/java/com/booktable/controller/AuthController.java
+++ booktable-backend/src/main/java/com/booktable/controller/AuthController.java
@@ -1,14 +1,20 @@
 package com.booktable.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.booktable.dto.RegisterRequest;
+import com.booktable.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
-    @GetMapping("/api/hello2")
-    public String sayHello() {
-        return "Hello from BookTable!";
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        return authService.registerUser(request);
     }
 }
--- booktable-backend/src/main/java/com/booktable/service/AuthService.java
+++ booktable-backend/src/main/java/com/booktable/service/AuthService.java
@@ -1,4 +1,45 @@
 package com.booktable.service;
 
+import com.booktable.dto.RegisterRequest;
+import com.booktable.model.AuthProvider;
+import com.booktable.model.Role;
+import com.booktable.model.User;
+import com.booktable.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public ResponseEntity<String> registerUser(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body("User with this email already exists.");
+        }
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRoles(Collections.singleton(Role.CUSTOMER));  // Default to CUSTOMER role
+        user.setProvider(AuthProvider.LOCAL);
+
+        userRepository.save(user);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body("User registered successfully.");
+    }
 }
