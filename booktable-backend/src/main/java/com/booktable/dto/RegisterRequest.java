c0c960dc7c7825b9b92e40aa052e2c7523253901
Ashish Bhusal <ashish.bhusal.16999@gmail.com>
2025-04-11 15:38:35 -0700

    Backend: Remove the Role field from RegisterRequest DTO
    User should not dictate their role during signup. Allowing users to
    dictate their role defeats the purpose of having roles at the first place.

--- booktable-backend/src/main/java/com/booktable/dto/RegisterRequest.java
+++ booktable-backend/src/main/java/com/booktable/dto/RegisterRequest.java
@@ -1,14 +1,10 @@
 package com.booktable.dto;
 
-import com.booktable.model.Role;
 import lombok.Data;
-
-import java.util.Set;
 
 @Data
 public class RegisterRequest {
     private String name;
     private String email;
     private String password;
-    private Set<Role> roles;
 }
