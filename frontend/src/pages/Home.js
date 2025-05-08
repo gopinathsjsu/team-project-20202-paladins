df849cd3e4fbb844e68c8b582af5dcf407ed4307
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-05-08 03:57:04 -0700

    load restaurants

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { fetchRestaurants } from "../redux/slices/restaurantSlice";
 import {
