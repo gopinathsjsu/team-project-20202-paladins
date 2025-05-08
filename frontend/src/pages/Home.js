73caa09020d6fafeacb1ca8296a2b0e49b3d5b93
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-05-08 08:16:37 -0700

    ci fix

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { fetchRestaurants, resetRestaurants } from "../redux/slices/restaurantSlice";
+import { fetchRestaurants } from "../redux/slices/restaurantSlice";
 import {
   Box,
   Typography,
